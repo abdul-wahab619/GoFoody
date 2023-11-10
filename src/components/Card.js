b51147f2489d47bd296c8c0016e81a5a9a348ce8
Abdul Wahab <abdulwahab@gmail.com>
2023-11-10 22:37:58 +0500

    create a simple nav card footer components

---
+++ src/components/Card.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <div className="card mt-3" style={{ width: "18rem", maxHeight: "360px" }}>
+        <img
+          src="https://source.unsplash.com/random/600x400/?spicy-tikka"
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title.
+          </p>
+          <div className="container w-100">
+            <select className="m-2 h-100 bg-success rounded">
+              {Array.from(Array(6), (e, i) => {
+                return (
+                  <option key={i + 1} value={i + 1}>
+                    {i + 1}
+                  </option>
+                );
+              })}
+            </select>
+            <select className="m-2 h-100 bg-success rounded">
+              <option value="half">Half</option>
+              <option value="full">Full</option>
+            </select>
+            <div className="d-inline h-100 fs-5">Total Price</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
