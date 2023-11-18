001c4bcc3a0ee1602b8ebb548be68d9efadd5706
Abdul Wahab <abdulwahab@gmail.com>
2023-11-18 15:39:51 +0500

    fix the height of images

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,12 @@ const Card = (props) => {
   return (
     <div>
       <div className="card mt-3" style={{ width: "18rem", maxHeight: "360px" }}>
-        <img src={props.imgSrc} className="card-img-top" alt="..." />
+        <img
+          src={props.imgSrc}
+          className="card-img-top"
+          alt="..."
+          style={{ height: "150px", objectFit: "fill" }}
+        />
         <div className="card-body">
           <h5 className="card-title">{props.foodName}</h5>
           <p className="card-text">
@@ -24,8 +29,12 @@ const Card = (props) => {
               })}
             </select>
             <select className="m-2 h-100 bg-success rounded">
-              {priceOptions.map((data)=> {
-                return <option key={data} value={data}>{data}</option>
+              {priceOptions.map((data) => {
+                return (
+                  <option key={data} value={data}>
+                    {data}
+                  </option>
+                );
               })}
             </select>
             <div className="d-inline h-100 fs-5">Total Price</div>
