9faf223f8a2b513cf53dd264617d7d79fee3f043
Abdul Wahab <abdulwahab@gmail.com>
2023-11-18 15:16:39 +0500

    remove carousel file and use directly in Home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Card from "../components/Card";
-import Carousel from "../components/Carousel";
 
 const Home = () => {
   const [search, setSearch] = useState("");
